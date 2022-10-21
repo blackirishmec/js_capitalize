1d5f955a3b1dd1658ab00099724e911e75831d21
Michael Coughlin <63324746+blackirishmec@users.noreply.github.com>
2022-10-21 13:17:14 -0400

    Add files via upload

---
+++ js_capitalize.js
@@ -0,0 +1,23 @@
+function capitalize() {
+    let words_in_string = this.split(" ");
+    
+    if(words_in_string.length != 0)
+    {
+        words_in_string.forEach((word,word_index) => {
+            let lowercase_word = word.LowerCase();
+
+            let capitalized_first_letter = word.charAt(0).toUpperCase();
+
+            let capitalized_word = capitalized_first_letter + lowercase_word.slice(1);
+
+            this.capitalized += capitalized_word;
+
+            if((word_index + 1) < words_in_string.length)
+            {
+                this.capitalized += " ";
+            }
+        });
+    }
+
+    return this.capitalized;
+}
